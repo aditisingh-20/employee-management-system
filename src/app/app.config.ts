f47d57892f33691eb226c4caef4037faf8dc0e91
Singh <aditisingh77@deloitte.com>
2024-03-25 10:56:46 +0530

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"employee-management-syst-37815","appId":"1:588363285147:web:c8d4636a29c98134c67c3e","storageBucket":"employee-management-syst-37815.appspot.com","apiKey":"AIzaSyDjUYZyxkYCZfWXxXmUOagxLwCWzQP8ROQ","authDomain":"employee-management-syst-37815.firebaseapp.com","messagingSenderId":"588363285147"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminDetailsComponent } from './admin-details/admin-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo:'/login',pathMatch:'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'admin-home', component: AdminHomeComponent},
+    {path: 'admin-home/details/:username', component: AdminDetailsComponent},
+];
